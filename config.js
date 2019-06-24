ad28258b15a79d742bc4ff43ce859e0de0f6b7e3
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-06-24 12:15:17 +0530

    added iste config

---
+++ config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Grayscale', // <title>
+  manifestName: 'Grayscale',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-grayscale/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Anubhav Srivastava',
+  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/in/theanubhav/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook-f',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+  ],
+  email: 'anubhav.srivastava00@gmail.com',
+  phone: '000-00000',
+  address: '305 Del Sol, Sunnyvale, CA',
+};
