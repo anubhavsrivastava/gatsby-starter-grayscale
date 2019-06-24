029ab652542c872f4455e05d81d14f5cca62e2cf
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-06-24 12:16:44 +0530

    added pages

---
+++ src/pages/404.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+const IndexPage = () => (
+  <Layout>
+    <div className="container-fluid p-0">
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="about"
+      >
+        <header className="major">
+          <h2>
+            Page not found
+            <br />
+            Not a valid URL
+          </h2>
+        </header>
+      </section>
+    </div>
+  </Layout>
+);
+
+export default IndexPage;
---
+++ src/pages/index.js
@@ -0,0 +1,125 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import Scroll from '../components/Scroll';
+
+import { Link } from 'gatsby';
+import config from '../../config';
+import Footer from '../components/Footer';
+import SocialLinks from '../components/SocialLinks';
+import Subscribe from '../components/Subscribe';
+import Header from '../components/Header';
+
+import ipad from '../assets/images/ipad.png';
+import demoImage1 from '../assets/images/demo-image-01.jpg';
+import demoImage2 from '../assets/images/demo-image-02.jpg';
+import bgMaster from '../assets/images/bg-masthead.jpg';
+
+const IndexPage = () => (
+  <Layout>
+    <Header />
+
+    <header className="masthead">
+      <div className="container d-flex h-100 align-items-center">
+        <div className="mx-auto text-center">
+          <h1 className="mx-auto my-0 text-uppercase">{config.heading}</h1>
+          <h2 className="text-white-50 mx-auto mt-2 mb-5">
+            {config.subHeading}
+          </h2>
+          <Scroll type="id" element="about">
+            <a href="#about" className="btn btn-primary">
+              About
+            </a>
+          </Scroll>
+        </div>
+      </div>
+    </header>
+
+    <section id="about" className="about-section text-center">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 mx-auto">
+            <h2 className="text-white mb-4">Built with Bootstrap 4</h2>
+            <p className="text-white-50">
+              Grayscale is a free Bootstrap theme created by Start Bootstrap. It
+              can be yours right now, simply download the starter on
+              <a href="https://github.com/anubhavsrivastava/gatsby-starter-grayscale">
+                the github repo
+              </a>
+              .
+            </p>
+          </div>
+        </div>
+        <img src={ipad} className="img-fluid" alt="" />
+      </div>
+    </section>
+
+    <section id="projects" className="projects-section bg-light">
+      <div className="container">
+        <div className="row align-items-center no-gutters mb-4 mb-lg-5">
+          <div className="col-xl-8 col-lg-7">
+            <img className="img-fluid mb-3 mb-lg-0" src={bgMaster} alt="" />
+          </div>
+          <div className="col-xl-4 col-lg-5">
+            <div className="featured-text text-center text-lg-left">
+              <h4>Shoreline</h4>
+              <p className="text-black-50 mb-0">
+                Grayscale is open source and MIT licensed. This means you can
+                use it for any project - even commercial projects! Download it,
+                customize it, and publish your website!
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div className="col-lg-6">
+            <img className="img-fluid" src={demoImage1} alt="" />
+          </div>
+          <div className="col-lg-6">
+            <div className="bg-black text-center h-100 project">
+              <div className="d-flex h-100">
+                <div className="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 className="text-white">Misty</h4>
+                  <p className="mb-0 text-white-50">
+                    An example of where you can put an image of a project, or
+                    anything else, along with a description.
+                  </p>
+                  <hr className="d-none d-lg-block mb-0 ml-0" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center no-gutters">
+          <div className="col-lg-6">
+            <img className="img-fluid" src={demoImage2} alt="" />
+          </div>
+          <div className="col-lg-6 order-lg-first">
+            <div className="bg-black text-center h-100 project">
+              <div className="d-flex h-100">
+                <div className="project-text w-100 my-auto text-center text-lg-right">
+                  <h4 className="text-white">Mountains</h4>
+                  <p className="mb-0 text-white-50">
+                    Another example of a project with its respective
+                    description. These sections work well responsively as well,
+                    try this theme on a small screen!
+                  </p>
+                  <hr className="d-none d-lg-block mb-0 mr-0" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <Subscribe />
+
+    <SocialLinks />
+    <Footer />
+  </Layout>
+);
+
+export default IndexPage;
